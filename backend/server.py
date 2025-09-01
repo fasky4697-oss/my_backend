13b5d60417785bf18137ac6a801fe8f6421bc877
fasky4697-oss <fasky4697@gmail.com>
2025-09-02 00:49:40 +0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,10 @@
 MONGO_URL="mongodb://localhost:27017"
 DB_NAME="test_database"
 CORS_ORIGINS="*"
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello FastAPI!"}
